bf76c567af4180eaae051d90c00d7f3ccd6af043
sospinai7 <sospinai@eafit.edu.co>
2022-05-26 19:07:56 -0500

    nuevo log con hora y fecha

--- kernel.py
+++ kernel.py
@@ -4,7 +4,7 @@ from ctypes.wintypes import HLOCAL
 from io import StringIO
 from subprocess import call
 import subprocess
-import os, signal
+import os, signal, datetime
 
 kernel = Blueprint('kernel', __name__,
                         template_folder='templates',
@@ -14,37 +14,64 @@ kernel = Blueprint('kernel', __name__,
 def show():
     return render_template('app3.html')
 
+def new_log(log):
+    add_log = log
+    with open('myfile.txt', 'a+') as fp:
+        fp.write(add_log)
+        fp.close()
+        pass
 
 @kernel.route('/open_app/<string:slug>/<int:number>', methods=['GET', 'POST'])
 def open_app(slug, number=None):
+    calc_is_open = False
+    notepad_is_open = False
+    paint_is_open = False
+    
     if request.method == 'GET':
+        time = datetime.datetime.now()
         if slug == "notepad":
             app_name = "notepad.exe"
+            
             if number == 1:
-                os.system('notepad.exe')
-                print("open notepaad")
+                if notepad_is_open == False:
+                    new_log("[{}/{}/{} - {}:{}:{}] open notepad\n".format(str(time.day), str(time.month), str(time.year), str(time.hour), str(time.minute), str(time.second)))
+                    os.system('notepad.exe')
+                    print("open notepaad")
+                    notepad_is_open = True
             elif number == 2:
+                new_log("[{}/{}/{} - {}:{}:{}] close notepad\n".format(str(time.day), str(time.month), str(time.year), str(time.hour), str(time.minute), str(time.second)))
                 command = "taskkill /IM {} /F".format(str(app_name))
                 os.system(command)
                 print("close notepad")
+                notepad_is_open = False
         elif slug == "calc":
             app_name = '"Calculator.exe"'
             if number == 1:
-                os.system('calc.exe') 
-                print("open calc1")
+                if calc_is_open == False:
+                    new_log("[{}/{}/{} - {}:{}:{}] open calc\n".format(str(time.day), str(time.month), str(time.year), str(time.hour), str(time.minute), str(time.second)))
+                    os.system('calc.exe') 
+                    print("open calc1")
+                    calc_is_open = True
             elif number == 2:
+                new_log("[{}/{}/{} - {}:{}:{}] close calc\n".format(str(time.day), str(time.month), str(time.year), str(time.hour), str(time.minute), str(time.second)))
                 command = "taskkill /IM {} /F".format(str(app_name))
                 os.system(command)
                 print("close clac1")
+                calc_is_open = False
         elif slug == "paint":
             app_name = '"mspaint.exe"'
             if number == 1:
-                call(['mspaint']) 
-                print("open paint")
+                if paint_is_open == False:
+                    new_log("[{}/{}/{} - {}:{}:{}] open paint\n".format(str(time.day), str(time.month), str(time.year), str(time.hour), str(time.minute), str(time.second)))
+                    call(['mspaint']) 
+                    print("open paint")
+                    paint_is_open = True
             elif number == 2:
+                new_log("[{}/{}/{} - {}:{}:{}] close paint\n".format(str(time.day), str(time.month), str(time.year), str(time.hour), str(time.minute), str(time.second)))
                 command = "taskkill /IM {} /F".format(str(app_name))
                 os.system(command)
                 print("close paint")
+                paint_is_open = False
 
         return render_template('index.html')
 
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import sys
+import sys, os
 
 from kernel import kernel
 
@@ -8,7 +8,44 @@ app.register_blueprint(kernel, url_prefix='/kernel')
 
 posts = []
 
+###
+ #  Create txt
+###
+cwd = os.getcwd()
+dir_name = cwd
+listOfFiles = os.listdir(dir_name)
+txt_name = "\myfile.txt"                            ##Nombre del archivo del logs
+txt_file_path = cwd+txt_name
+file_exists = os.path.exists(txt_file_path)
+
+def rename_txt(new_name):
+    new_txt_name = cwd+new_name
+    # txt_name = new_name
+    print(new_txt_name)
+    os.rename(txt_file_path, new_txt_name)
+
+def delete_file():
+    for file in listOfFiles:
+        if file.endswith(".txt"):
+            os.remove(os.path.join(dir_name, file))
+    create_txt("Gestor de archivos\n") 
+    # rename_txt('\prueba.txt')                     ##Para cambiar el nombre del archivo .txt
+
+def create_txt(log):
+    with open('myfile.txt', 'w') as fp:
+        fp.write(log)
+        # fp.close()
+        pass
+
+def txt_file():
+    for file in listOfFiles:
+        if file.endswith(".txt"):
+            delete_file()
+        else:
+            create_txt("Gestor de archivos\n")
+
 @app.route("/")
 def index():
     print("inicio el programa")
+    txt_file()
     return render_template("index.html", num_posts=len(posts))
